51dcacfa5f59cfa379fa955cf9160461f2672b39
Typhoonic <koxxor12@gmail.com>
2025-07-24 22:52:26 +0200

    import Redux. Create store and initialize data.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Hero from "./components/Hero/Hero";
 import List from "./components/List/List";
 import SearchForm from "./components/SearchForm/SearchForm";
 import Container from "./components/Container/Container";
+import Button from "./components/Button/Button";
+import TextInput from "./components/TextInput/TextInput";
 function App() {
   return (
     <Container>
       <Hero />
       <SearchForm />
       <List />
+      <TextInput placeholder='Napisz mi coś'></TextInput>
+      <Button>Mała</Button>
     </Container>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
